3e64d2c9d95e47f5ccd0d9794139010bf73314de
dok4ever <girlsfrontline45@gmail.com>
2025-11-29 00:20:18 +0800

    添加Bluetooth功能

--- configuration.nix
+++ configuration.nix
@@ -26,6 +26,11 @@
   networking.hostName = "dok4ever"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  # 配置蓝牙
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
